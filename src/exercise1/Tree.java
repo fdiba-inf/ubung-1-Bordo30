7fac39c470b91fb56ff657ac74d43e8296be5687
Bordo30 <boris.dobrev30@gmail.com>
2021-10-12 11:39:47 +0300

    Done and dusted!

---
+++ src/exercise1/Tree.java
@@ -0,0 +1,15 @@
+package exercise1;
+
+public class Tree {
+
+    public static void main(String[] args) {
+        char star = '*';
+
+        System.out.print("\n" + "\t" + star);
+        System.out.print("\n" + "   " + star + star + star);
+        System.out.print("\n" + "  " + star + star + star + star + star);
+        System.out.print("\n" + " " + star + star + star + star + star + star + star);
+        System.out.print("\n" + star + star + star + star + star + star + star + star + star);
+        System.out.print("\n" + "   " + star + star + star);
+    }
+}
